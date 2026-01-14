2f38dabf147107870496753513756efbe5d7cee4
Your Name <you@example.com>
2026-01-14 10:02:12 +0300

    Remove link request

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -705,6 +705,44 @@
 						</Tooltip>
 					</div>
 				{/if}
+
+				{#if $user?.role === 'admin' || $user?.permissions?.workspace?.requests}
+					<div class="">
+						<Tooltip content={"Запросы"} placement="right">
+						<a
+							class=" cursor-pointer flex rounded-xl hover:bg-gray-100 dark:hover:bg-gray-850 transition group"
+							href="/workspace/requests"
+							on:click={async (e) => {
+							e.stopImmediatePropagation();
+							e.preventDefault();
+
+							goto('/workspace/requests');
+							itemClickHandler();
+							}}
+							aria-label={"Запросы"}
+							draggable="false"
+						>
+							<div class=" self-center flex items-center justify-center size-9">
+							<svg
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+								stroke-width="1.5"
+								stroke="currentColor"
+								class="size-4.5"
+							>
+								<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z"
+								/>
+							</svg>
+							</div>
+						</a>
+						</Tooltip>
+					</div>
+    		{/if}
+
 			</div>
 		</button>
 
@@ -799,7 +837,7 @@
 						id="sidebar-webui-name"
 						class=" self-center font-medium text-gray-850 dark:text-white font-primary"
 					>
-						{$WEBUI_NAME}
+						{"INTACT"}
 					</div>
 				</a>
 				<Tooltip
@@ -935,6 +973,41 @@
 							</a>
 						</div>
 					{/if}
+
+					{#if $user?.role === 'admin' || $user?.permissions?.workspace?.requests}
+      <div class="px-[0.4375rem] flex justify-center text-gray-800 dark:text-gray-200">
+       <a
+        id="sidebar-requests-button"
+        class="grow flex items-center space-x-3 rounded-2xl px-2.5 py-2 hover:bg-gray-100 dark:hover:bg-gray-900 transition"
+        href="/workspace/requests"
+        on:click={itemClickHandler}
+        draggable="false"
+        aria-label="Запросы"
+       >
+        <div class="self-center">
+         <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="2"
+          stroke="currentColor"
+          class="size-4.5"
+         >
+          <path
+           stroke-linecap="round"
+           stroke-linejoin="round"
+           d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z"
+          />
+         </svg>
+        </div>
+
+        <div class="flex self-center translate-y-[0.5px]">
+         <div class=" self-center text-sm font-primary">Запросы</div>
+        </div>
+       </a>
+      </div>
+     {/if}
+
 				</div>
 
 				{#if ($models ?? []).length > 0 && (($settings?.pinnedModels ?? []).length > 0 || $config?.default_pinned_models)}
