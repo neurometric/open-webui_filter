79b5949cb327dc062e6e2ae41243c1bf1af27bce
GongniR <dpakhomov@facemetric.ru>
2026-03-10 20:37:24 +0300

    add test env params

--- sanitizer/app.py
+++ sanitizer/app.py
@@ -28,12 +28,12 @@ def _env_bool(name: str, default: str = "0") -> bool:
 
 
 NER_ENABLED = _env_bool("NER_ENABLED", "1")
-NER_URL = os.getenv("NER_URL", "").strip()
+NER_URL = os.getenv("NER_URL", "http://172.31.4.20:8002/ner").strip()
 NER_TIMEOUT_MS = int(os.getenv("NER_TIMEOUT_MS", "2000"))
 NER_MIN_SCORE = float(os.getenv("NER_MIN_SCORE", "0.65"))
 NER_LABELS = {
     x.strip().upper()
-    for x in os.getenv("NER_LABELS", "PER,ORG,LOC").split(",")
+    for x in os.getenv("NER_LABELS", "PER,ORG,LOC,FIRST_NAME,LAST_NAME,MIDDLE_NAME").split(",")
     if x.strip()
 }
 logging.info(NER_LABELS)
